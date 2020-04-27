b9051d5579090e9a9c8cb7f529b43835b0f6cd92
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-04-27 00:48:37 -0600

    new gun and some scripts update

---
+++ New Unity Project/Assets/Gun.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float count = 0f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range);
+    }
+
+
+}
